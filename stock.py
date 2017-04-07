4a0485c5a5d06c3f7f1df790efe54fff745e0814
bg <bg@example.com>
2017-04-07 19:58:05 +0800

    增加广东peg股

--- stock.py
+++ stock.py
@@ -5,9 +5,11 @@ import sendMail
 
 class stock():
     def __init__(self):
-        self.getThingsEveryday()
         self.__yearRange = 5        #5年 从2012到2016
         self.__yearBegin = datetime.now().year-self.__yearRange
+        #self.__getYearReportOnline()
+
+        self.getThingsEveryday()
         #print(yearbegin)
         self.__pdYearReport = [pd.read_excel('D:/zzz/joinquant/data4stock/' + \
                     str(self.__yearBegin+i) + 'y.xls','Report',na_values=['NA']) \
@@ -33,18 +35,23 @@ class stock():
         #print(self.__pdYearReportNear[self.__pdYearReportNear.code==600522])
         #print(self.__pdYearReportNear[self.__pdYearReportNear.code == 600067])
         #pdtest.to_excel('D:/zzz/joinquant/data4stock/' + str('test') + 'Profit.xls', sheet_name='Profit')
+        print('\n')
 
     def stockNotNow(self):
         return self.__stocksNotNow
 
     def getThingsEveryday(self):
         yearEnd = datetime.now().year-1
+
+
         pdProfitLastYear = ts.get_profit_data(yearEnd, 4)
         pdProfitLastYear.to_excel('D:/zzz/joinquant/data4stock/' + \
                     str(yearEnd) + 'Profit.xls', sheet_name='Profit')
         pdReportLastYear = ts.get_report_data(yearEnd, 4)
         pdReportLastYear.to_excel('D:/zzz/joinquant/data4stock/' + \
                     str(yearEnd) + 'y.xls', sheet_name='Report')
+
+
         self.__stockBasics = ts.get_stock_basics()  #获得昨天pe
         self.__stockBasics['code']=self.__stockBasics.index.astype(int)
         self.__stockBasics.sort_index(inplace=True)
@@ -53,10 +60,10 @@ class stock():
         self.__pdForwardEps = pd.merge(self.__stockBasics, self.__stockTodayAll, on='code')
         self.__pdForwardEps['feps'] = self.__pdForwardEps['settlement']/self.__pdForwardEps['pe']
         print('\n')
-        #print(self.__pdForwardEps[self.__pdForwardEps.code == 600522])
+        #print(self.__pdForwardEps[self.__pdForwardEps.code == 601200])
 
     def peg_stock(self):
-        self.per = 10
+        self.per = 20
         for i in range(self.__yearRange):
             self.__pdYearReport[i].rename(columns={'profits_yoy': 'yoy'+str(i)}, inplace = True)
         self.pdYear4Report = self.__pdYearReport[0].copy()
@@ -64,45 +71,88 @@ class stock():
             self.pdYear4Report = self.pdYear4Report.merge(self.__pdYearReport[i],on='code')
 
         YP4 = self.pdYear4Report
-        czg = YP4[(YP4.yoy0 > self.per) & (YP4.yoy1 > self.per)
+        czg = YP4[(YP4.yoy1 > self.per)
                 & (YP4.yoy2 > self.per) & (YP4.yoy3 > self.per)]
 
         stockHaveReportLastYear = set(self.__pdYearReport[4].code.values)
 
         stockSet = set(czg['code'].values)
+        stocksCZG = []
+        stocksGD = []
+        dy = ts.get_area_classified()
         for stock in stockSet:
+            if self.__pdForwardEps[self.__pdForwardEps.code == stock].empty:
+                continue
             pe = self.peNow(stock)
             if stock not in self.__stocksNow:
                 inc = YP4[YP4.code == stock].yoy3.values[0]
+                if dy[dy.code == ('%06d' % stock)]['area'].values[0] == '广东':
+                    if pe < 50 and peg > 0 and peg < 2:
+                        stocksGD.append(['%06d' % stock, YP4[YP4.code == stock].name_x.values[0][0], \
+                                         round(pe, 2), round(inc, 2), round(peg, 2),
+                                         dy[dy.code == ('%06d' % stock)]['area'].values[0]])
             else:   #去年年报已出
                 inc = self.__pdYearReport[4][self.__pdYearReport[4].code == stock].yoy4.values[0]
             peg = pe/inc
-            if pe < 30 and peg > 0 and peg < 0.46:
-                    print('%06d %s pe %.2f, inc %.2f%%,peg %.2f' %(stock, YP4[YP4.code == stock].name_x.values[0][0], pe, inc, peg))
-
+            if pe < 30 and peg > 0 and peg < 0.8:
+                #print('%06d %s pe %.2f, inc %.2f%%,peg %.2f' %(stock, YP4[YP4.code == stock].name_x.values[0][0], pe, inc, peg))
+                stocksCZG.append(['%06d' % stock, YP4[YP4.code == stock].name_x.values[0][0], \
+                                      round(pe, 2), round(inc, 2), round(peg, 2), dy[dy.code == ('%06d' % stock)]['area'].values[0] ])
+        #print(stocksCZG)
+        columns = ['code', 'name', 'pe', 'inc', 'peg', 'area']
+        pdCZG = pd.DataFrame(data = stocksCZG, columns=columns)
+        pdCZG.sort_values(by=['peg'], inplace=True)
+        print(pdCZG)
         forecastSet = set(self.__forecastLastYear['code'].values)
         #print('002841')
         #print(YP4[YP4.code == 2841])
-        print(forecastSet & stockSet)
-        print('forecast')
+        #print(forecastSet & stockSet)
+        stocksFCZG = []
+        #print(dy[dy.code.isin(fpegStock)])
         for stock in stockSet:
+            if self.__pdForwardEps[self.__pdForwardEps.code == stock].empty:
+                continue
+
             if stock in forecastSet:
                 #print('%06d, range %.2f' % (stock, self.__forecastLastYear[self.__forecastLastYear.code == stock]['range'].values[0]))
                 pe = self.peNow(stock)
-                inc = self.__forecastLastYear[self.__forecastLastYear.code == stock]['range'].values[0]
-                peg = pe/inc
-                if pe < 50 and peg > 0 and peg < 0.46:
-                    print('%06d %s pe %.2f, inc %.2f%%,peg %.2f' %(stock, YP4[YP4.code == stock].name_x.values[0][0], pe, inc, peg))
+                inc = float(self.__forecastLastYear[self.__forecastLastYear.code == stock]['range'].values[0])
+
+                peg = pe / inc
+                if pe < 50 and peg > 0 and peg < 0.8:
+                    #print('%06d %s pe %.2f, inc %.2f%%,peg %.2f' %(stock, YP4[YP4.code == stock].name_x.values[0][0], pe, inc, peg))
+                    stocksFCZG.append(['%06d' % stock, YP4[YP4.code == stock].name_x.values[0][0], \
+                                      round(pe, 2), round(inc, 2), round(peg, 2), dy[dy.code == ('%06d' % stock)]['area'].values[0] ])
+                if dy[dy.code == ('%06d' % stock)]['area'].values[0] == '广东':
+                    stocksGD.append(['%06d' % stock, YP4[YP4.code == stock].name_x.values[0][0], \
+                                      round(pe, 2), round(inc, 2), round(peg, 2), dy[dy.code == ('%06d' % stock)]['area'].values[0] ])
+
+        #print(stocksFCZG)
+        pdFCZG = pd.DataFrame(data=stocksFCZG, columns=columns)
+        pdFCZG.sort_values(by=['peg'], inplace=True)
+        pdGD = pd.DataFrame(data=stocksGD, columns=columns)
+        pdFCZG.sort_values(by=['peg'], inplace=True)
+        print(pdFCZG)
+        print('GD')
+        print(pdGD)
         #pd.merge()
         #print(self.__pdYearReport)
+        print(pdCZG['code'].values)
+        print(pdFCZG['code'].values)
 
-    def __getProfitOnline(self):
+    def __getYearReportOnline(self):
         yearbegin = datetime.now().year-self.__yearRange
         pdYearProfit = [ts.get_profit_data(yearbegin+i,4) \
                         for i in range(self.__yearRange)]
         for i in range(self.__yearRange):
-            pdYearProfit[0].to_excel('D:/zzz/joinquant/data4stock/' + \
+            pdYearProfit[i].to_excel('D:/zzz/joinquant/data4stock/' + \
                     str(yearbegin+i) + 'Profit.xls', sheet_name='Profit')
+
+        pdReportLastYear = [ts.get_report_data(yearbegin+i,4) \
+                        for i in range(self.__yearRange)]
+        for i in range(self.__yearRange):
+            pdReportLastYear[i].to_excel('D:/zzz/joinquant/data4stock/' + \
+                    str(yearbegin+i) + 'y.xls', sheet_name='Report')
 
     def pickHHCG(self):
         #gross_profit_rate 毛利率
@@ -131,28 +181,32 @@ class stock():
         if datetime.now().weekday() < 5:
             stockPdNow = self.get_today_all()
             stockPdNow['code'] = stockPdNow['code'].astype(int)
+            #print(stockPdNow)
             #print(stockPdNow[stockPdNow.code == stock])
+            if stockPdNow[stockPdNow.code == stock].empty:
+                return self.__stockBasics[self.__stockBasics.code == stock]['pe'].values[0]
             priceNow = stockPdNow[stockPdNow.code == stock]['trade'].values[0]
             if priceNow == 0:
                 return self.__stockBasics[self.__stockBasics.code == stock]['pe'].values[0]
+
             forwardEps = self.__pdForwardEps[self.__pdForwardEps.code == stock]['feps'].values[0]
             return round(priceNow/forwardEps,2)
         else:
             return self.__stockBasics[self.__stockBasics.code == stock]['pe'].values[0]
 
-        def ai_gzscx(self):         #分配预案的次新股
-            df = ts.profit_data(top=1000, year=datetime.now().year-1)
-            #df = df[df.shares >= 1]
-            df.sort_values(by='shares', ascending=False)
-            df['code'] = df['code'].astype(int)
-            dfCX = set(df['code'].values)
-            pd = ts.get_industry_classified()
-            pd['code'] = pd['code'].astype(int)
-            # print(pd[pd.c_name == '次新股'])
-            # print(set(pd['c_name'].values))
-            setCX = set(pd[pd.c_name == '次新股']['code'].values)
-            print('\n')
-            print(df[df.code.isin(setCX)])
+    def ai_gzscx(self):         #分配预案的次新股
+        df = ts.profit_data(top=1000, year=datetime.now().year-1)
+        #df = df[df.shares >= 1]
+        df.sort_values(by='shares', ascending=False)
+        df['code'] = df['code'].astype(int)
+        dfCX = set(df['code'].values)
+        pd = ts.get_industry_classified()
+        pd['code'] = pd['code'].astype(int)
+        # print(pd[pd.c_name == '次新股'])
+        # print(set(pd['c_name'].values))
+        setCX = set(pd[pd.c_name == '次新股']['code'].values)
+        print('\n')
+        print(df[df.code.isin(setCX)])
 
 #pd2014y = ts.get_report_data(2014,4)
 #pd2014y.to_excel('D:/zzz/joinquant/data4stock/2014y.xls', sheet_name='Report')
@@ -161,10 +215,12 @@ class stock():
 #os.path.exists('D:/zzz/joinquant/data4stock/2014y.xls')
 #pd2016y = pd.read_excel('D:/zzz/joinquant/data4stock/2016y.xls','Report',na_values=['NA'])
 #print(pd2016y.code.values)
-'''
+
+
 s = stock()
 s.peg_stock()
-'''
+
+
 #pdF = ts.forecast_data(2016, 4)
 #stockList = set(pdF['code'].values.astype(int))
 '''
